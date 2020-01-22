3e8f352a354c760320ed8e2b165e93fb8f8e8f6c
qiuzj <zhanjia21@163.com>
2020-01-22 21:38:39 +0800

    二叉树遍历注释

--- src/main/tree/org/javaee/tree/traversal/binarytree/BinaryTreeTraversal.java
+++ src/main/tree/org/javaee/tree/traversal/binarytree/BinaryTreeTraversal.java
@@ -3,8 +3,23 @@ package org.javaee.tree.traversal.binarytree;
 import org.javaee.tree.BinaryNode;
 
 /**
- * 二叉树的三种遍历方式
+ * <pre>
+ * 二叉树的三种遍历方式。
  * 
+ * 理解：每次递归进入函数，传入的节点理解为一棵新的子树，传进来的节点可以理解为新的根节点。
+ * 先序遍历递推公式：1.先访问根节点 2.遍历左子树 3.遍历右子树。对于第二步、第三步的左右子树遍历，其实就是重复前面的三个步骤。
+ * 
+ * 
+前序遍历的递推公式：
+preOrder(r) = print r->preOrder(r->left)->preOrder(r->right)
+
+中序遍历的递推公式：
+inOrder(r) = inOrder(r->left)->print r->inOrder(r->right)
+
+后序遍历的递推公式：
+postOrder(r) = postOrder(r->left)->postOrder(r->right)->print r
+
+ * </pre>
  * @author 二进制之路
  *
  */
